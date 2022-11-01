c13582dfd24a016e859301df23c4e2f98a6e74f4
Nikita N <nikitanikolin85@gmail.com>
2022-11-02 02:05:21 +0300

    adedd styling and created repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import github from "./assets/github.png";
+
 import "./App.css";
 
 function App() {
@@ -12,18 +14,43 @@ function App() {
     });
   }, []);
   return (
-    <div className="App">
+    <div className="conatainer">
       <h1>my repos</h1>
       {projectData.map((item, index) => {
         return (
-          <div key={index} id={item.id}>
+          <div className="wrapper" key={index} id={item.id}>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
-            {item.topics.map((item, key)=>{
-              return <span key={key}>{item}</span>
+            {item.topics.map((item, key) => {
+              return (
+                <span className="topics" key={key}>
+                  {item}
+                </span>
+              );
             })}
             {/* TODO: return languages with values */}
-            <a href={item.html_url}>link</a>
+            <div className="line-chart">
+              <span className="green-background"></span>
+              <span className="red-background"></span>
+              <span className="orange-background"></span>
+            </div>
+            <ul>
+              <li>
+                <span className="circle"></span>
+                javascript 84%
+              </li>
+              <li>
+                <span className="circle"></span>
+                html 11%
+              </li>
+              <li>
+                <span className="circle"></span>
+                css 5%
+              </li>
+            </ul>
+            <a href={item.html_url}>
+              <img src={github} alt="" />
+            </a>
           </div>
         );
       })}
